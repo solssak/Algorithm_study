eba6b8845ad3cce2d9449151cbd4c48318213bb1
solssak <gswoong1012@gmail.com>
2023-05-08 18:17:40 +0900

    Day01_같은 숫자는 싫어

---
+++ 230508.py
@@ -0,0 +1,10 @@
+# 같은 숫자는 싫어
+
+def solution(arr):
+    answer = [arr[0]]
+
+    for i in range(1, len(arr)):
+        if arr[i] != answer[-1]:
+            answer.append(arr[i])
+        
+    return answer
